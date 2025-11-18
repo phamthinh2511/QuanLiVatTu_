07fdd99dbad25907611c6a5af3eb2198ec8b06ac
phamthinh2511 <flare.dsly@gmail.com>
2025-11-18 17:52:12 +0700

    Hoàn Thành SlidebarMenu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,47 @@ namespace PageNavigation
         {
             InitializeComponent();
         }
-        public void CloseApp_Click(object sender, RoutedEventArgs e)
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(togglebutton.IsChecked == true)
+            {
+                tt_Home.Visibility = Visibility.Collapsed;
+                tt_VatTu.Visibility = Visibility.Collapsed;
+                tt_KhachHang.Visibility = Visibility.Collapsed;
+                tt_NhapKho.Visibility = Visibility.Collapsed;
+                tt_XuatKho.Visibility = Visibility.Collapsed;
+                tt_VanChuyen.Visibility = Visibility.Collapsed;
+                tt_Setting.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_Home.Visibility = Visibility.Visible;
+                tt_VatTu.Visibility = Visibility.Visible;
+                tt_KhachHang.Visibility = Visibility.Visible;
+                tt_NhapKho.Visibility = Visibility.Visible;
+                tt_XuatKho.Visibility = Visibility.Visible;
+                tt_VanChuyen.Visibility = Visibility.Visible;
+                tt_Setting.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void togglebutton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Pages.Opacity = 1;
+        }
+
+        private void togglebutton_Checked(object sender, RoutedEventArgs e)
+        {
+            Pages.Opacity = 0.3;
+        }
+
+        private void contentuser_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            togglebutton.IsChecked = false;
+        }
+
+        private void closebutton_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
