8db92af97bdabda298d3b32ae9d7f253eb15bd54
tientruong222 <truongnhattien222@gmail.com>
2025-12-03 11:31:26 +0700

    Update PhieuNhapVatTuVM.cs

--- ViewModel/PhieuNhapVatTuVM.cs
+++ ViewModel/PhieuNhapVatTuVM.cs
@@ -12,56 +12,7 @@ namespace PageNavigation.ViewModel
     {
 		public PhieuNhapVatTuVM()
 		{
-			ListDetail = new ObservableCollection<CT_PhieuNhapVatTuVM>();
-			GRNDate = DateOnly.FromDateTime(DateTime.Now);
 		}
-		// grn = goods receive note
-		private int _grnID;
-
-		public int GRNID
-		{
-			get { return _grnID; }
-			set { _grnID = value; OnPropertyChanged(); }
-		}
-		private DateOnly _grnDate;
-
-		public DateOnly GRNDate
-		{
-			get { return _grnDate; }
-			set { _grnDate = value; OnPropertyChanged(); }
-		}
-		private decimal _grnCost;
-
-		public decimal GRNCost
-		{
-			get { return _grnCost; }
-			set { _grnCost = value; OnPropertyChanged(); }
-		}
-		public ObservableCollection<CT_PhieuNhapVatTuVM> ListDetail { get; set; }
-		public void UpdateTotalCost()
-		{
-			GRNCost = ListDetail.Sum(item => item.TotalAmount);
-		}
-		public void AddDetail(int productID, string productName, int providerID, decimal inputPrice)
-		{
-			var newItem = new CT_PhieuNhapVatTuVM
-			{
-				ProductID = productID,
-				ProductName = productName,
-				ProviderID = providerID,
-				InputPrice = inputPrice,
-				Quantity = 1,
-				GRNID = this.GRNID
-			};
-			newItem.PropertyChanged += (sender, args) =>
-			{
-				if (args.PropertyName == nameof(CT_PhieuNhapVatTuVM.TotalAmount))
-				{
-					UpdateTotalCost();
-				}
-			};
-			ListDetail.Add(newItem);
-			UpdateTotalCost();
-		}
+		
 	}
 }
