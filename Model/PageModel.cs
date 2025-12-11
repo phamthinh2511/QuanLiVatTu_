b57a3feeb8e575e802e570de3e6776bffe6461db
phamthinh2511 <flare.dsly@gmail.com>
2025-12-11 09:28:22 +0700

    Cập Nhật Thuộc Tính

--- Model/PageModel.cs
+++ Model/PageModel.cs
@@ -8,15 +8,6 @@ namespace PageNavigation.Model
 {
     public class PageModel
     {
-        public int CustomerCount { get; set; }
-        public string ProductStatus { get; set; }
-        public DateOnly OrderDate { get; set; }
-        public decimal TransactionValue { get; set; }
-        public TimeOnly Ship_Delivery { get; set; }
-        public bool LocationStatus { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public string Gender { get; set; }
+
     }
 }
