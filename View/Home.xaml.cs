731e2421de9bad8fa421f3c62601cf3cf0be773f
phamthinh2511 <flare.dsly@gmail.com>
2026-01-18 14:16:42 +0700

    Hoàn Thành Biểu Đồ Tồn Kho

--- View/Home.xaml.cs
+++ View/Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using LiveCharts;
 using LiveCharts.Configurations;
+using PageNavigation.View.HomeUserControl;
 using PageNavigation.ViewModel;
 using System.Collections.ObjectModel;
 using System.Windows;
@@ -15,35 +16,15 @@ namespace PageNavigation.View
     /// </summary>
     public partial class Home : UserControl
     {
-        public ChartValues<double> DoanhThuValues { get; set; }
-        public ObservableCollection<string> Labels { get; set; }
-        public object ColorMapper { get; set; }
+        
         public Home()
         {
             InitializeComponent();
-            var dangerBrush = new SolidColorBrush(Color.FromRgb(238, 83, 83));
-            var blueBrush = new SolidColorBrush(Color.FromRgb(74, 144, 226)); 
-            var yellowBrush = new SolidColorBrush(Color.FromRgb(255, 206, 84));
-            var purpleBrush = new SolidColorBrush(Color.FromRgb(148, 0, 211)); 
-
-            var colors = new List<Brush> { dangerBrush, blueBrush, yellowBrush, purpleBrush };
-            ColorMapper = Mappers.Xy<double>()
-                .X(value => value)
-                .Y((value, index) => index)
-                .Fill((value, index) => colors[index % colors.Count])
-                .Stroke((value, index) => colors[index % colors.Count]);
-            DoanhThuValues = new ChartValues<double> { 10, 50, 39, 50 };
-            Labels = new ObservableCollection<string> { "Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4" };
+            this.DataContext = new HomeVM();
             var vatTuVM = new VatTuVM();
             var hoaDonVM = new HoaDonVM();
-            Binding myBinding = new Binding("TongDoanhThu");
-            DoanhThuButton.number.SetBinding(TextBlock.TextProperty, myBinding);
-            DoanhThuButton.DataContext = hoaDonVM;
-            chart.DataContext = this;
             lvSanPham.DataContext = vatTuVM;
-            DoanhThuButton.title.Text = "Doanh Thu";
-            DoanhThuButton.title.Foreground = Brushes.Violet;
-            DoanhThuButton.picture.Source = new BitmapImage(new Uri("/Images/cashhome.png", UriKind.Relative));
+          
         }
 
         
--- ViewModel/HomeVM.cs
+++ ViewModel/HomeVM.cs
@@ -1,13 +1,39 @@
-﻿using System;
+﻿using PageNavigation.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using PageNavigation.Utilities;
 namespace PageNavigation.ViewModel
 {
-    public class HomeVM
+    public class HomeVM : ViewModelBase
     {
+        private decimal _doanhThuThangNay;
+        public decimal DoanhThuThangNay
+        {
+            get { return _doanhThuThangNay; }
+            set { _doanhThuThangNay = value; OnPropertyChanged(); }
+        }
+        private void LoadDoanhThu()
+        {
 
+            var today = DateTime.Now;
+
+            using (var context = new QuanLyVatTuContext())
+            {
+                var doanhThu = context.HoaDon
+                    .Where(x => x.NgayLapHoaDon.Value.Month == today.Month && x.NgayLapHoaDon.Value.Year == today.Year)
+                    .Sum(x => (decimal?)x.TongTien) ?? 0;
+
+                DoanhThuThangNay = doanhThu;
+            }
+        }
+        public HomeVM()
+        {
+            LoadDoanhThu();
+        }
     }
 }
